5661d626c475fa1bcba328528b067538e17e17ae
victorpizzarro <victor.pizzarrorj@gmail.com>
2026-04-06 17:14:12 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/enerz/enerz/EnerzApplication.java
@@ -0,0 +1,13 @@
+package com.enerz.enerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnerzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnerzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enerz/enerz/EnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enerz.enerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
